03960c8dc61e197ac01c9fc25d6cce462212dce5
Karsenh <KarsenHansen@gmail.com>
2021-06-01 03:05:45 -0700

    Added error and loading state - blank render(?)

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from './Context/AuthContext';
 
 export default function Signup() {
@@ -7,11 +7,26 @@ export default function Signup() {
   const passRef = useRef();
   const passConfirmRef = useRef();
   const { signup } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    signup(emailRef.current.value, passRef.current.value);
+    // Validation checks
+    if (passRef.current.value !== passConfirmRef.current.value) {
+      // Set an error with new state
+      return setError('Passwords do not match');
+    }
+
+    try {
+      setError('');
+      setLoading(true);
+      await signup(emailRef.current.value, passRef.current.value);
+    } catch {
+      setError('Failed to create user');
+    }
+    setLoading(false);
   }
 
   return (
@@ -19,7 +34,8 @@ export default function Signup() {
       <Card>
         <Card.Body>
           <h2 className='text-center mb-4'>Sign Up</h2>
-          <Form>
+          {error && <Alert variant='danger'>Error</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id='email'>
               <Form.Label>Email</Form.Label>
               <Form.Control type='email' ref={emailRef} required />
@@ -32,7 +48,8 @@ export default function Signup() {
               <Form.Label>Email</Form.Label>
               <Form.Control type='password' ref={passConfirmRef} required />
             </Form.Group>
-            <Button className='w-100' type='submit'>
+            {/* Set disabled state to same bool value as 'if loading' */}
+            <Button disabled={loading} className='w-100' type='submit'>
               Sign Up
             </Button>
           </Form>
