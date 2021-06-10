51736cf243d3afc076629de938f24e4f1115aeda
Karsenh <KarsenHansen@gmail.com>
2021-06-09 19:22:33 -0700

    Added 'Update Profile' button link to Dashboard component

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Card, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 export default function Dashboard() {
@@ -17,6 +18,9 @@ export default function Dashboard() {
           {error && <Alert variant='danger'>{error}</Alert>}
           <strong>Email: </strong>
           {currentUser.email}
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update Profile
+          </Link>
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
